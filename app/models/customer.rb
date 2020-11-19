9589d7e0ca2652a5e4f32247acc98a28752142aa
Daniel Lee <dlee@sheetmusicplus.com>
2020-11-19 02:30:06 -0800

    Add customer-order relationship

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :orders
+
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   subject { build(:customer) }
 
+  describe 'relationships' do
+    it { is_expected.to have_many(:orders) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
